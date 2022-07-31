a522292a4c3737409806e1faba343b8033f2eb72
Joanne <>
2022-07-31 17:09:47 +0800

    add cors everywhere proxy

--- src/redux/actions/certAction.ts
+++ src/redux/actions/certAction.ts
@@ -19,31 +19,38 @@ const issueCertAction = makeActionCreator(ISSUE_CERT);
 const clearCertAction = makeActionCreator(CLEAR_CERT);
 const issueCertSuccessAction = makeActionCreator(ISSUE_CERT_SUCCESS, "data");
 const issueCertFailAction = makeActionCreator(ISSUE_CERT_FAIL, "error");
+const corsEverywherePrefix = "https://cors-anywhere.herokuapp.com/";
 
 // api calls
 const getCertificate = () => {
   return (dispatch: any, getState: () => RootStateProps) => {
     const { username } = getState()?.login;
     dispatch(getCertAction());
-    const url = `http://107.20.26.70/api/certs/?userId=${username}`;
-    axios.get(url).then(
-      (resp) => {
-        console.log("??? resp", resp);
-        const { status, data } = resp || {};
-        if (status === 200) {
-          return dispatch(getCertSuccessAction(data));
-        }
-        dispatch({ type: GET_CERT_FAIL, errorCode: status });
-      },
-      (err) => dispatch({ type: GET_CERT_FAIL, err })
-    );
+    const url = `${corsEverywherePrefix}http://107.20.26.70/api/certs/?userId=${username}`;
+    axios
+      .get(url, {
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      })
+      .then(
+        (resp) => {
+          console.log("??? resp", resp);
+          const { status, data } = resp || {};
+          if (status === 200) {
+            return dispatch(getCertSuccessAction(data));
+          }
+          dispatch({ type: GET_CERT_FAIL, errorCode: status });
+        },
+        (err) => dispatch({ type: GET_CERT_FAIL, err })
+      );
   };
 };
 
 const issueCertificate = (studentId: string, certDataString: string) => {
   return (dispatch: any) => {
     dispatch(issueCertAction());
-    const url = `http://107.20.26.70/api/certs/`;
+    const url = `${corsEverywherePrefix}http://107.20.26.70/api/certs/`;
     const bodyFormData = new FormData();
     bodyFormData.append("certDataString", certDataString);
     bodyFormData.append("lastUpdatedAt", moment().toISOString());
@@ -55,16 +62,23 @@ const issueCertificate = (studentId: string, certDataString: string) => {
     if (!studentId || !certDataString) {
       return dispatch(issueCertFailAction("missing param"));
     }
-    axios.post(url, bodyFormData, { timeout: 60000 }).then(
-      (resp) => {
-        const { status, data } = resp || {};
-        if (status === 200) {
-          return dispatch(issueCertSuccessAction(data));
-        }
-        dispatch(issueCertFailAction(status));
-      },
-      (err) => dispatch(issueCertFailAction(err))
-    );
+    axios
+      .post(url, bodyFormData, {
+        timeout: 60000,
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      })
+      .then(
+        (resp) => {
+          const { status, data } = resp || {};
+          if (status === 200) {
+            return dispatch(issueCertSuccessAction(data));
+          }
+          dispatch(issueCertFailAction(status));
+        },
+        (err) => dispatch(issueCertFailAction(err))
+      );
   };
 };
 
